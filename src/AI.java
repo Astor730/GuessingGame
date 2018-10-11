97b2d59745776658c194308b53cebc494745d6be
BT_1N3_08 <mister.p730@gmail.com>
2018-10-11 14:48:10 -0400

    Beginning of objective 2

--- src/AI.java
+++ src/AI.java
@@ -3,8 +3,22 @@ import java.util.Scanner;
 public class AI {
     public static void main(String[] args)
     {
+        String userin = "";
+        Scanner usertype = new Scanner(System.in);
         System.out.println("Guess a number between 1 and 100");
+        while (userin != "correct")
+        {
+            System.out.println("Is your number 50");
+            userin = usertype.nextLine();
+            if (userin.equals("higher"))
+            {
 
+            }
+            else if(userin.equals("lower"))
+            {
+
+            }
+        }
     }
 
 }
