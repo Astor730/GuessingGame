a0df578ac7c1cc8fff7e8e6504cb8fbcbc7ff1ee
Astor730 <42044772+Astor730@users.noreply.github.com>
2018-10-11 23:01:38 -0400

    Beginning of Method 2

--- src/AI.java
+++ src/AI.java
@@ -4,21 +4,32 @@ public class AI {
     public static void main(String[] args)
     {
         String userin = "";
+        int guess = 50;
         Scanner usertype = new Scanner(System.in);
         System.out.println("Guess a number between 1 and 100");
-        while (userin != "correct")
+        System.out.println("Type correct, higher or lower to continue the code correctly");
+        while (userin.equalsIgnoreCase("correct") != true)
         {
-            System.out.println("Is your number 50");
+            System.out.println("Is your number " + guess + "?");
             userin = usertype.nextLine();
-            if (userin.equals("higher"))
+            if(userin.equalsIgnoreCase("correct"))
             {
-
+                System.out.println("I guessed correctly");
             }
-            else if(userin.equals("lower"))
+            else if(userin.equalsIgnoreCase("higher"))
             {
-
+                guess = 75;
+            }
+            else if(userin.equalsIgnoreCase("lower"))
+            {
+                guess = 25;
+            }
+            else
+            {
+                System.out.println("That's not a correct input");
             }
         }
+
     }
 
 }
