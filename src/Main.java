327f5f92f8f2c30eb4496108601d2495bec3eeeb
BT_1N3_08 <mister.p730@gmail.com>
2018-10-12 14:34:48 -0400

    Guessing Game with name

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
             int guess = 0;
+            String name = "";
             int guessnum = 1;
             double x = Math.random();
             int r = 0;
             Scanner input = new Scanner(System.in);
-            System.out.println("How difficult would you like the game to be.");
+            System.out.println("What is your name?");
+            name = input.nextLine();
+            System.out.println("How difficult would you like the game to be " + name);
             System.out.println("Press 1 for easy, 2 for medium, 3 for hard and 4 for impossible");
             while(guess != 1 && guess !=2 && guess != 3 && guess !=4) {
                 guess = input.nextInt();
                 if (guess == 1) {
                     r = (int) (x * 11);
-                    System.out.println("Guess a number between 0 and 10");
+                    System.out.println("Guess a number between 0 and 10 " + name);
                 } else if (guess == 2) {
                     r = (int) (x * 101);
-                    System.out.println("Guess a number between 0 and 100");
+                    System.out.println("Guess a number between 0 and 100 " + name);
                 } else if (guess == 3) {
                     r = (int) (x * 1001);
-                    System.out.println("Guess a number between 0 and 1000");
+                    System.out.println("Guess a number between 0 and 1000 " + name);
                 } else if (guess == 4) {
                     r = (int) (x * 10001);
-                    System.out.println("Guess a number between 0 and 10,000");
+                    System.out.println("Guess a number between 0 and 10,000 " + name);
                 } else {
                     System.out.println("Incorrect input");
                 }
@@ -35,6 +38,6 @@ public class Main {
                 guess = input.nextInt();
             }
             System.out.println("Congrats you guessed correctly");
-            System.out.println("It took you " + guessnum + " guesses");
+            System.out.println("It took you " + guessnum + " guesses " + name);
     }
 }
