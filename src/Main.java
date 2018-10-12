5650ae406083b822b5ebb3e2b1854e88eb81683c
BT_1N3_08 <mister.p730@gmail.com>
2018-10-12 14:29:40 -0400

    Guessing Game with difficulties

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,28 @@ public class Main {
             int guess = 0;
             int guessnum = 1;
             double x = Math.random();
-            int r = (int) (x * 11);
+            int r = 0;
             Scanner input = new Scanner(System.in);
-            System.out.println("Guess a number between 0 and 10");
+            System.out.println("How difficult would you like the game to be.");
+            System.out.println("Press 1 for easy, 2 for medium, 3 for hard and 4 for impossible");
+            while(guess != 1 && guess !=2 && guess != 3 && guess !=4) {
+                guess = input.nextInt();
+                if (guess == 1) {
+                    r = (int) (x * 11);
+                    System.out.println("Guess a number between 0 and 10");
+                } else if (guess == 2) {
+                    r = (int) (x * 101);
+                    System.out.println("Guess a number between 0 and 100");
+                } else if (guess == 3) {
+                    r = (int) (x * 1001);
+                    System.out.println("Guess a number between 0 and 1000");
+                } else if (guess == 4) {
+                    r = (int) (x * 10001);
+                    System.out.println("Guess a number between 0 and 10,000");
+                } else {
+                    System.out.println("Incorrect input");
+                }
+            }
             guess = input.nextInt();
             while (guess != r)
             {
