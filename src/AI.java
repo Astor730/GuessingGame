025a78c6ffb067a821011134fa46c5a88fb7e8ef
Astor730 <42044772+Astor730@users.noreply.github.com>
2018-10-13 13:59:44 -0400

    AI complete

--- src/AI.java
+++ src/AI.java
@@ -5,25 +5,40 @@ public class AI {
     {
         String userin = "";
         int guess = 50;
-        int lowerlimit = 50;
+        int guessnum = 1;
+        String name = "";
+        int lowerlimit = 0;
         int upperlimit = 100;
-        int lowerlimit2 = 0;
-        int upperlimit2 = 50;
         Scanner usertype = new Scanner(System.in);
-        System.out.println("Guess a number between 1 and 100");
+        System.out.println("What's your name");
+        name = usertype.nextLine();
+        System.out.println("Guess a number between 1 and 100 " + name);
         System.out.println("Type correct, higher or lower to continue the code correctly");
-        while (userin.equalsIgnoreCase("correct"))
+        while (userin.equalsIgnoreCase("correct") != true)
         {
             System.out.println("Is your number" + guess);
+            userin = usertype.nextLine();
+            if(guess == 99)
+            {
+                guess = 100;
+                guessnum++;
+            }
             if (userin.equalsIgnoreCase("higher"))
             {
-                lowerlimit = (upperlimit - lowerlimit) + lowerlimit;
+                lowerlimit = guess;
+                upperlimit = lowerlimit + (upperlimit-lowerlimit);
+                guess = guess+ ((int) (upperlimit - lowerlimit)/2);
+                guessnum++;
             }
             else if(userin.equalsIgnoreCase("lower"))
             {
-
+                upperlimit = guess;
+                lowerlimit = upperlimit - (lowerlimit+upperlimit);
+                guess = guess - ((int) (upperlimit - lowerlimit)/2);
+                guessnum++;
             }
         }
+        System.out.println("It took me " + guessnum + " guess " + name);
 
     }
 
